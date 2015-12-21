b5c96c101e5b78d2d391cfaf67a6211adaab1c72
Andrew Ross <andrew.ross@kurtosys.com>
2015-12-21 11:50:49 +0200

    I think I've fixed the ambient module thing.

--- expect.d.ts
+++ expect.d.ts
@@ -1,5 +1,5 @@
 
-declare module "expect" {
+declare module expect {
 
     interface IExpectation<TExpected> {
         /**
@@ -227,8 +227,7 @@ declare module "expect" {
          */
         extend(extension: Object);
     }
-
-
-    let expect: IExpect;
-    export default expect;
 }
+
+declare let expected: expect.IExpect;
+export = expected;
